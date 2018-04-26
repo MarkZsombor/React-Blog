4182cd107e8e86ce537eed98e2ef6e3dd21523b1
Mark Zsombor <markszsombor@gmail.com>
2018-04-26 14:17:00 -0700

    restarted using create react app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <a href="/auth/google">Login HERE</a>
+      </div>
+    );
+  }
+}
+
+export default App;
