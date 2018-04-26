84bf710c5920e6078a4fbaf01a9cf24af676b5e0
Mark Zsombor <markszsombor@gmail.com>
2018-04-25 21:19:43 -0700

    added logout route

--- api-server.js
+++ api-server.js
@@ -11,7 +11,7 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(cookieSession({
-  maxAge: 30 * 24 * 60 * 60 * 1000, //30days
+  maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
   keys: [keys.cookieKey],
 }));
 
