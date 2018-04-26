854936c6ed03f0fcc810e44af801b7f459353c67
Mark Zsombor <markszsombor@gmail.com>
2018-04-26 16:11:25 -0700

    added reducers

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,9 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 // import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(),
+);
 
 ReactDOM.render(
   <Provider store={store}>
