dbee82390452abd3d840298db5dff5954cd7ec34
Mark Zsombor <markszsombor@gmail.com>
2018-04-25 19:07:30 -0700

    installed mongoose, set mlab db

--- api-server.js
+++ api-server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./server/config/keys');
 require('./server/services/passport');
 
-const PORT = 5000;
-// const PORT = process.env.PORT || 5000;
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 require('./server/routes/authRoutes')(app);
+
+const PORT = 5000;
+// for some reason this keeps defaulting to 5100
+// const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
